ae4c2ed8068c6abe0d8658cefd5973723ba3f8c1
eric <ekchea@gmail.com>
2017-09-23 12:03:40 -0400

    Websocket connection through wss.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker.js';
 
 // Feed socket
 // const feedSocket = 'ws://127.0.0.1:8000/feed/';
-const feedSocket = 'ws://eldoradoapi.herokuapp.com/feed/';
+const feedSocket = 'wss://eldoradoapi.herokuapp.com/feed/';
 
 // Render the components, picking up where react left off on the server
 const element = <Filing socket={feedSocket}/>;
