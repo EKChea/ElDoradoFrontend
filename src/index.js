e921858b495e95888af45306104b98cd83f4b4ba
eric <ekchea@gmail.com>
2017-09-23 11:53:37 -0400

    Initial commit of SEC Feeds ReactJS application.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Filing from './components/Filing.js';
+import registerServiceWorker from './registerServiceWorker.js';
+
+
+// Feed socket
+// const feedSocket = 'ws://127.0.0.1:8000/feed/';
+const feedSocket = 'ws://eldoradoapi.herokuapp.com/feed/';
+
+// Render the components, picking up where react left off on the server
+const element = <Filing socket={feedSocket}/>;
+
+ReactDOM.render(element, document.getElementById('root'));
+
+registerServiceWorker();
