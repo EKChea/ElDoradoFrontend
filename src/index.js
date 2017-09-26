5264ac2560df67b4a4ced3a80ca5fa4648beff44
eric <ekchea@gmail.com>
2017-09-26 15:37:54 -0400

    Add ability to disrciminate between prod and local apis.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Filing from './components/Filing.js';
 import registerServiceWorker from './registerServiceWorker.js';
 
+import Filing from './components/Filing.js';
+import { getSocketUrl } from './utils.js'
 
-// Feed socket
-// const feedSocket = 'ws://127.0.0.1:8000/feed/';
-const feedSocket = 'wss://eldoradoapi.herokuapp.com/feed/';
+const socket = getSocketUrl();
 
 // Render the components, picking up where react left off on the server
-const element = <Filing socket={feedSocket}/>;
+const element = <Filing socket={socket}/>;
 
 ReactDOM.render(element, document.getElementById('root'));
 
